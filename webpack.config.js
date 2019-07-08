15e9c6900e36a0efe52ce62a7f9bc5c89c763633
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-08 15:49:01 +0300

    Set up production build

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
     }),
       new HtmlPlugin({
         template: 'src/index.html'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
       })
   ]
 };
